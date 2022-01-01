ccf2d4e431deb4737295c133895b471acc91a440
Matthew Nitschke <matthewcnitschke@gmail.com>
2022-01-01 11:10:02 -0700

    fixes

--- install.sh
+++ install.sh
@@ -2,14 +2,8 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-pushd
-
-cd $SCRIPT_DIR
-
 cp reservoir.service /etc/systemd/system
 
 EXEC_PATH="$(pwd)/start.sh"
 
 sed -i "s/<path to exec>/$EXEC_PATH/g" /etc/systemd/system/reservoir.service
-
-popd
