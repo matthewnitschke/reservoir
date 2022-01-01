ef28f3e0b07626ca8fd106195fba35d49d3e66ca
Matthew Nitschke <matthewcnitschke@gmail.com>
2022-01-01 11:20:23 -0700

    fixed?

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ cp reservoir.service /etc/systemd/system
 
 EXEC_PATH="$(pwd)/start.sh"
 
-sed -i '' "s+PATH_TO_EXEC+$EXEC_PATH+g" /etc/systemd/system/reservoir.service
+# Note, this will only work on GNU systems, NOT OSX
+sed -i "s+PATH_TO_EXEC+$EXEC_PATH+g" /etc/systemd/system/reservoir.service
